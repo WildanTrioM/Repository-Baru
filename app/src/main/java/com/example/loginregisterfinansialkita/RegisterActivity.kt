8c90d6fe635d37aaf049cebe90d64b8aac840ec3
WildanTrioM <wildantrio40@gmail.com>
2023-06-30 22:25:42 +0700

    Repository-Baru

---
+++ app/src/main/java/com/example/loginregisterfinansialkita/RegisterActivity.kt
@@ -0,0 +1,16 @@
+package com.example.loginregisterfinansialkita
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.activity.ComponentActivity
+
+class RegisterActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+
+    fun sendMessage(view: View){
+
+    }
+}
